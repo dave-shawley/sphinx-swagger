8e1b3f90ffd691930f2cb92914f31628c472f869
Dave Shawley <daveshawley@gmail.com>
2016-06-06 07:38:53 -0400

    Metadata updates for first release.

--- setup.py
+++ setup.py
@@ -24,10 +24,22 @@ def read_requirements(name):
 setuptools.setup(
     name='sphinx-swagger',
     version=sphinxswagger.__version__,
+    url='https://github.com/dave-shawley/sphinx-swagger',
     description='Generate swagger definitions from sphinx docs',
     long_description='\n'+open('README.rst').read(),
     author='Dave Shawley',
     author_email='daveshawley+python@gmail.com',
     packages=['sphinxswagger'],
     install_requires=read_requirements('installation.txt'),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Sphinx :: Extension',
+    ],
 )
--- sphinxswagger/__init__.py
+++ sphinxswagger/__init__.py
@@ -1,4 +1,4 @@
-version_info = (0, 0, 0)
+version_info = (0, 0, 1)
 __version__ = '.'.join(str(v) for v in version_info)
 
 
